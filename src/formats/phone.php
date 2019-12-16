e3308fa70a45d8bc574a0d8bf6d3af96e335a758
Roni Estein <roni.estein@gmail.com>
2019-12-16 15:00:21 -0600

    Added base phone helper
    WIP Need a facade, but get it working with alpha sem-ver

--- src/formats/phone.php
+++ src/formats/phone.php
@@ -1,24 +1,51 @@
 <?php
 
-
-function onlyDigits($text)
-{
-    return preg_replace('/[^0-9]/', '', $text);
-}
-
-function checkPhoneLength($number)
-{
-    return ((strlen($number) == 11 && $number[0] == "1") || (strlen($number) == 10 && $number[0] != "1"));
-}
-
-function formatPhone($number)
-{
-    $number = onlyDigits($number);
-    if ( ! checkPhoneLength($number)) {
-        throw new Exception('invalid phone number: *' . $number . '*', 0);
+if (! function_exists('onlyDigits')) {
+    /**
+     * Remove all non digit charaters from a string
+     *
+     * @param string $text
+     *
+     * @return string
+     */
+    function onlyDigits($text) : string
+    {
+        return preg_replace('/[^0-9]/', '', $text);
     }
-    
-    return '(' . substr($number, -10, 3) . ') '
-        . substr($number, -7, 3) . '-'
-        . substr($number, -4, 4);
+}
+
+if (! function_exists('checkPhoneLength')) {
+    /**
+     * Determines if a phone number is a legal length North America
+     *
+     * @param string $number
+     *
+     * @return bool
+     */
+    function checkPhoneLength($number) : bool
+    {
+        return ((strlen($number) == 11 && $number[0] == "1") || (strlen($number) == 10 && $number[0] != "1"));
+    }
+}
+
+if (! function_exists('formatPhone')) {
+    /**
+     * formats a phone number
+     *
+     * @param string $number
+     *
+     * @throws Exception
+     * @return string
+     */
+    function formatPhone($number) : string
+    {
+        $number = onlyDigits($number);
+        if ( ! checkPhoneLength($number)) {
+            throw new Exception('invalid phone number: *' . $number . '*', 0);
+        }
+        
+        return '(' . substr($number, -10, 3) . ') '
+            . substr($number, -7, 3) . '-'
+            . substr($number, -4, 4);
+    }
 }
